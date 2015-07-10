ef03a84c1eeb6fddac4a1e97a7336ce2981f0792
BestKora <developer@bestkora.com>
2015-07-10 19:10:39 +0300

    Unwind

---
+++ Smashtag/RootTweetTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RootTweetTableViewController.swift
+//  Smashtag
+//
+//  Created by Tatiana Kornilova on 7/10/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class RootTweetTableViewController: TweetTableViewController {
+    // необходимо для unwind segue
+    @IBAction func backToRoot(segue: UIStoryboardSegue) {}
+
+}
